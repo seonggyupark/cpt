2d77c1bcaf598323b02df5fc7160770cc06ad5b7
Tommaso Teofili <tommaso.teofili@gmail.com>
2011-12-14 20:10:57 +0100

    fixed characters encoding in budget annotator and removed dummy tags from pom file

--- cpt-uima/budget_annotator/src/main/java/cybion/budget_annotator/BudgetAnnotator.java
+++ cpt-uima/budget_annotator/src/main/java/cybion/budget_annotator/BudgetAnnotator.java
@@ -1,10 +1,12 @@
 package cybion.budget_annotator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import cybion.annotator_utils.AnnotatorUtils;
+import cybion.annotator_utils.DefaultTermsDictionaryProvider;
+import cybion.annotator_utils.DictionaryProvider;
+import cybion.annotator_utils.TermsDictionary;
+import cybion.annotator_utils.annotator.CybionAbstractAnnotator;
+import cybion.uima.ts.BandoEntity;
+import cybion.uima.ts.BudgetAnnotation;
 import org.apache.uima.SentenceAnnotation;
 import org.apache.uima.UimaContext;
 import org.apache.uima.cas.CASException;
@@ -13,32 +15,29 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
 
-import cybion.annotator_utils.AnnotatorUtils;
-import cybion.annotator_utils.DefaultTermsDictionaryProvider;
-import cybion.annotator_utils.DictionaryProvider;
-import cybion.annotator_utils.TermsDictionary;
-import cybion.annotator_utils.annotator.CybionAbstractAnnotator;
-import cybion.uima.ts.BandoEntity;
-import cybion.uima.ts.BudgetAnnotation;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BudgetAnnotator extends CybionAbstractAnnotator {
 
-  private static final String SIMPLE_BUDGET_REGEX = "(VAT|(B|b)udget|(A|a)ward):\\s*\\({0,1}\\s*(USD|Û|euro|$|pound|Euro|EURO|EUR|£|Pound|Dollar|Dollars){0,1}\\s*\\){0,1}\\s*(\\d{1,}\\.{0,1}\\,{0,1}\\s{0,1}){1,}\\s*\\({0,1}\\s*(USD|Û|euro|$|pound|Euro|EURO|EUR|£|Pound|Dollar|Dollars){0,1}\\s*\\){0,1}\\s*(per award){0,1}";
+  private static final String SIMPLE_BUDGET_REGEX = "(VAT|(B|b)udget|(A|a)ward):\\s*\\({0,1}\\s*(USD|â‚¬|euro|$|pound|Euro|EURO|EUR|Â£|Pound|Dollar|Dollars){0,1}\\s*\\){0,1}\\s*(\\d{1,}\\.{0,1}\\,{0,1}\\s{0,1}){1,}\\s*\\({0,1}\\s*(USD|â‚¬|euro|$|pound|Euro|EURO|EUR|Â£|Pound|Dollar|Dollars){0,1}\\s*\\){0,1}\\s*(per award){0,1}";
 
   private static final Pattern simpleBudgetPattern = Pattern.compile(SIMPLE_BUDGET_REGEX);
 
   private DictionaryProvider budgetProvider;
-  
+
   @Override
   public void initialize(UimaContext aContext) throws ResourceInitializationException {
     super.initialize(aContext);
     List<TermsDictionary> supportedDictionaries = new ArrayList<TermsDictionary>();
     supportedDictionaries.add(new BudgetDictionary("it"));
     supportedDictionaries.add(new BudgetDictionary("en"));
-    this.budgetProvider = new DefaultTermsDictionaryProvider(supportedDictionaries );
+    this.budgetProvider = new DefaultTermsDictionaryProvider(supportedDictionaries);
   }
 
-  public void extractWithCustomAlgorithm(JCas cas){
+  public void extractWithCustomAlgorithm(JCas cas) {
     boolean found = false;
     String document = cas.getDocumentText();
     Matcher matcher = simpleBudgetPattern.matcher(document);
@@ -76,7 +75,7 @@ public class BudgetAnnotator extends CybionAbstractAnnotator {
   @Override
   protected String[] getFeatureKeys(String language) {
     List<String> dictionaryItems = this.budgetProvider.getDictionary(language).getDictionaryItems();
-return dictionaryItems.toArray(new String[dictionaryItems.size()]);
+    return dictionaryItems.toArray(new String[dictionaryItems.size()]);
   }
 
 }
